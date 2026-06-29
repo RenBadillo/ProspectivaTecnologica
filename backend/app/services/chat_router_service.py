845e4e254dfb418ac5ec97e81112cc8b502589fb
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-06-28 21:01:25 -0600

    cada que alguien pide el inventario se toma una foto.

--- backend/app/services/chat_router_service.py
+++ backend/app/services/chat_router_service.py
@@ -9,6 +9,9 @@ from app.services.meal_plan_service import MealPlanService
 from app.services.nutrition_service import NutritionService
 from app.services.recipe_service import RecipeService
 from app.services.shopping_service import ShoppingService
+from app.vision.vision_camera import visionCamera
+
+print(">>> CHAT_ROUTER_SERVICE CARGADO <<<")
 
 
 class ChatRouterService:
@@ -16,6 +19,7 @@ class ChatRouterService:
     registration_sessions = {}
 
     def __init__(self):
+        self.camera = visionCamera()
         self.inventory_service = InventoryService()
         self.inventory_analysis_service = InventoryAnalysisService()
         self.recipe_service = RecipeService()
@@ -170,6 +174,8 @@ class ChatRouterService:
             decision.get("intent", "general")
         )
 
+        print(f"Intent detectado:{intent}")
+
         entities = decision.get("entities", {}) or {}
 
         fallback_intent = self.classify(message)
@@ -249,7 +255,15 @@ class ChatRouterService:
             )
 
         elif intent == "inventory":
+
+            
+            #raise Exception("PRUEBA")
+
+            
             #aqui se tomará la foto
+            print("Se entró a inventory")
+            self.camera.capture()
+            print("Foto tomada")
             #se generará el json del vlm
             #se comparará con el json actual
             #se quitará la diferencia de la base de datos
--- backend/app/vision/vision_camera.py
+++ backend/app/vision/vision_camera.py
@@ -1,10 +1,38 @@
 import requests
+from pathlib import Path
+import time
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+IMAGE_PATH = BASE_DIR / "images" / "latest.jpg"
+
+
 class visionCamera:
+
+    def capture(self):
+
+        self.request_capture()
+        return self.wait_for_image()
+
+    def wait_for_image(self,timeout=10):
+
+        start = time.time()
+
+        while time.time() - start < timeout:
+
+            if IMAGE_PATH.exists() and IMAGE_PATH.stat().st_size > 0:
+                return IMAGE_PATH
+
+            time.sleep(0.1)
+
+        raise TimeoutError("No llegó la imagen.")
+
+
 
     def request_capture(self):
 
         response = requests.post(
-            "http://192.168.100.200/capture",
+            "http://192.168.100.78/capture",
             timeout=10
         )
 
@@ -13,6 +41,6 @@ class visionCamera:
         print(response.status_code)
         print(response.text)
 
-
-camera = visionCamera()
-camera.request_capture()
+if __name__ == "__main__":
+    vision = visionCamera()
+    vision.capture()
